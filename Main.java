b95946f6ad34216feeacd3583e5d4c8cb2c9348d
roy murray <80788182+RoyalRoys435@users.noreply.github.com>
2022-11-07 08:53:55 +0000

    added the switch cses

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
    Scanner sc = new Scanner(System.in);
     System.out.println("enter your age");
     int age = sc.nextInt();
-    sc.close();
+    
 
     int y = 5;
 
@@ -25,6 +25,49 @@ class Main {
     }
     else{
       System.out.println("your ugly");
+    }
+
+        System.out.println("\nWelcome to our Diner! please select n entree:\n"
+                      + "1) steak\n"
+                      + "2) wings\n"
+                      + "3) salad\n"
+                      + "and any other number for nothing!");
+
+    int choice = sc.nextInt();
+
+    switch(choice){
+      case 1:
+        System.out.println("here is your stake");
+        break;
+      case 2:
+        System.out.println("here are your wings");
+         break;
+      case 3:
+        System.out.println("here is your salad");
+        break;
+      default:
+        System.out.println("there is nothing that you might be interested in?");
+        break;
+    }
+     System.out.println("\nPlease select a drink:\n"
+                            + "1) iced tea\n"
+                            + "2) soda\n"
+                            + "3) lemonade\n"
+                            + "and any other number for nothing!");
+    int drink = sc.nextInt();
+    switch(drink){
+      case 1:
+        System.out.println("here is the iced tea you ordred");
+        break;
+      case 2:
+        System.out.println("here is the soda you orderd");
+        break;
+      case 3:
+        System.out.println("here is the lemonade you orderd");
+        break;
+      default:
+        System.out.println("there is no drink you might be interested in");
+        break;
     }
   }
 }
